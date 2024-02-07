74b39ff586ac06adf5faef0ac93289fdb0ca24ba
Kelsey Evans <thekelso91@gmail.com>
2024-02-07 12:49:25 -0600

    Changed code to say if guess was high or low

--- guess_number.py
+++ guess_number.py
@@ -4,11 +4,14 @@ print("I'm thinking of a number...")
 
 while True:
     guess = (input('What number am I thinking of? '))
-    if guess == number:
-        print('Congratulations! You guessed the right number.')
-        break
     if guess == 'q':
         print(f"That's okay, the number was {number}.")
         break
+    guess = int(guess)
+    if guess == number:
+        print('Congratulations! You guessed the right number.')
+        break
+    elif guess < number:
+        print('Sorry, that guess was too low. Try again.')
     else:
-        print(f'Sorry! The number that is the wrong number. Try again.')
+        print('Sorry, that guess was too high. Try again.')
