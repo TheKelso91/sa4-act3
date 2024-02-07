0fd0d61d2ba22e45a008fecc6581b555cb948fba
Kelsey Evans <thekelso91@gmail.com>
2024-02-07 13:03:44 -0600

    Changed code for guesses

--- guess_number.py
+++ guess_number.py
@@ -3,6 +3,9 @@ number = 10
 print("I'm thinking of a number...")
 
 while True:
+    max_guesses = 3
+    current_guesses = 0
+    
     guess = (input('What number am I thinking of? '))
     if guess == number:
         print('Congratulations! You guessed the right number.')
@@ -11,4 +14,8 @@ while True:
         print(f"That's okay, the number was {number}.")
         break
     else:
-        print(f'Sorry! The number that is the wrong number. Try again.')
+        current_guesses += 1
+        if max_guesses == current_guesses:
+            print(f'Sorry! The number that is the wrong number. You have run out of guesses.')
+        else:
+            print(f'Sorry, that number is wrong. You have {max_guesses - current_guesses} left.')
