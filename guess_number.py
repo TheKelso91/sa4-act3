51d6cc82e9346f1a410d9bf5e8b10d7865d91b09
Kelsey Evans <thekelso91@gmail.com>
2024-02-07 12:16:21 -0600

    Revised code for better usability

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,14 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input('What number am I thinking of? '))
 
-if guess == number:
-    print('Congratulations! You guessed the right number.')
-else:
-    print(f'Sorry! The number was {number}.')
+while True:
+    guess = (input('What number am I thinking of? '))
+    if guess == number:
+        print('Congratulations! You guessed the right number.')
+        break
+    if guess == 'q':
+        print(f"That's okay, the number was {number}.")
+        break
+    else:
+        print(f'Sorry! The number that is the wrong number. Try again.')
